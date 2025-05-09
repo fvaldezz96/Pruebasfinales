e7e4af2e68be2edc9e61b02f489189cad8d960db
fvaldezz96 <valdezfede21@gmail.com>
2025-05-09 01:06:09 -0300

    fix: agregue controlador crear producto

--- java-docker-project/src/main/java/com/example/controllers/ProductController.java
+++ java-docker-project/src/main/java/com/example/controllers/ProductController.java
@@ -13,6 +13,11 @@ public class ProductController {
         return "List of products";
     }
 
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
 //     private List<String> products = new ArrayList<>();
 // 
 //     @GetMapping
@@ -55,3 +60,51 @@ public class ProductController {
 //         }
 //     }
 }
+
+
+
+//CLASS CRUDS PRODUCT
+
+// package com.example.controllers;
+// 
+// import com.example.repositories.ProductRepository;
+// import models.Product;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
+// 
+// import java.util.List;
+// 
+// @RestController
+// @RequestMapping("/api/products")
+// public class ProductController {
+// 
+//     @Autowired
+//     private ProductRepository productRepository;
+// 
+//     @GetMapping
+//     public List<Product> getProducts() {
+//         return productRepository.findAll();
+//     }
+// 
+//     @PostMapping
+//     public Product addProduct(@RequestBody Product product) {
+//         return productRepository.save(product);
+//     }
+// 
+//     @GetMapping("/{id}")
+//     public Product getProductById(@PathVariable Long id) {
+//         return productRepository.findById(id).orElse(null);
+//     }
+// 
+//     @PutMapping("/{id}")
+//     public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+//         product.setId(id);
+//         return productRepository.save(product);
+//     }
+// 
+//     @DeleteMapping("/{id}")
+//     public String deleteProduct(@PathVariable Long id) {
+//         productRepository.deleteById(id);
+//         return "Product deleted with id: " + id;
+//     }
+// }
